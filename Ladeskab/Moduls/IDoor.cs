4fb0478dad35484fd74bfcc11d1a96cbc8e1b44c
Warmdahl <emilwarmdahl@gmail.com>
2020-03-17 08:55:48 +0100

    Door interface made

---
+++ Ladeskab/Moduls/IDoor.cs
@@ -0,0 +1,15 @@
+﻿namespace Ladeskab.Moduls
+{
+    public interface IDoor
+    {
+        //Tells if door is locked or unlocked
+        char state { get; }
+        
+        //locks the door
+        void Lock();
+        
+        //unlock the door
+        void unLock();
+
+    }
+}
