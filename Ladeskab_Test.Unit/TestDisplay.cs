976847e7edd3dd201b7306610b8b2e81a7914e8a
Alexander Hoffmann <Alexander.wh@hotmail.com>
2020-03-18 15:39:40 +0100

    Added TestRFIDReader og TestDisplay

---
+++ Ladeskab_Test.Unit/TestDisplay.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Ladeskab.Moduls;
+using NUnit.Framework;
+
+namespace Ladeskab_Test.Unit
+{
+    class TestDisplay
+    {
+        private Ladeskab.Moduls.Display _D;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _D = new Display();
+        }
+
+        // Test af User Instruction
+        [TestCase("Dette er en instruktion!")]
+        public void TestUserInstruction(string testString)
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                _D.DisplayUserInstructions(testString);
+                string expected = string.Format("Bruger instruktion: {0}{1}", testString, Environment.NewLine);
+                Assert.AreEqual(expected, sw.ToString());
+            }
+        }
+
+        // Test af Message
+        [TestCase("Dette er en besked!")]
+        public void TestChargingMessage(string testString)
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                _D.DisplayChargingMessage(testString);
+                string expected = string.Format("Opladnings besked: {0}{1}", testString, Environment.NewLine);
+                Assert.AreEqual(expected, sw.ToString());
+            }
+        }
+    }
+}
---
+++ Ladeskab_Test.Unit/TestRFIDReader.cs
@@ -0,0 +1,46 @@
+﻿using Ladeskab.Moduls;
+using NUnit.Framework;
+
+namespace RFIDReader.Unit
+{
+    [TestFixture]
+    class TestRFIDReader
+    {
+        private Ladeskab.Moduls.RFidReader _rFidReader;
+        private RFIDReadEventArgs _rfidReadEventArgs;
+        private int Valid_ID = 432;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _rfidReadEventArgs = null;
+            _rFidReader = new RFidReader();
+
+            _rFidReader.RFIDReadEvent += (o, args) => { _rfidReadEventArgs = args; };
+        }
+
+        // Test 1
+        [Test]
+        public void RFID_Reader_EventFired()
+        {
+            _rFidReader.SimulateReadRFID(432);
+            Assert.That(_rfidReadEventArgs.ID, Is.Not.Null);
+        }
+
+        // Test 2
+        [Test]
+        public void RFID_Reader_Succes()
+        {
+            _rFidReader.SimulateReadRFID(432);
+            Assert.That(_rfidReadEventArgs.ID, Is.EqualTo(Valid_ID));
+        }
+
+        // Test 3
+        [Test]
+        public void RFID_Reader_Failed()
+        {
+            _rFidReader.SimulateReadRFID(21);
+            Assert.That(_rfidReadEventArgs.ID, Is.Not.EqualTo(Valid_ID));
+        }
+    }
+}
