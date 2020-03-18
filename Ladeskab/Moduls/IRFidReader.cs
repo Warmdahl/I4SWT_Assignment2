f8a34ca79788d2699b51f1d2ee413c97811f1db5
Nicolai Dreves Nielsen <Nicolai_Dreves1994@hotmail.dk>
2020-03-18 08:38:00 +0100

    did something

---
+++ Ladeskab/Moduls/IRFidReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab.Moduls
+{
+    public interface IRFidReader
+    {
+        private event EventHandler<RFidChangedEventArgs> RFidChangedEvent;
+
+        void SetID(int newID);
+    }
+}
---
+++ Ladeskab/Moduls/RFidReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab.Moduls
+{
+    public class RFidReader : IRFidReader
+    {
+        private int id;
+
+        public event EventHandler<RFifChangedEventArgs> RFidChangedEvent;
+
+        public void SetID(int newID)
+        {
+            OnRFidChanged(new RFidChangedEventArgs {ID = newID});
+        }
+
+        public virtual void OnRFidChanged(RFidChangedEventArgs e)
+        {
+            RFidChangedEvent?.Invoke(this, e);
+        }
+
+        public class RFidChangedEventArgs : EventArgs
+        {
+            public int ID { get; set; }
+        }
+
+    }
+}
