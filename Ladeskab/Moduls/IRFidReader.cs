aad508d9872a699001acd642c4af40fe1b08f891
Nicolai Dreves Nielsen <Nicolai_Dreves1994@hotmail.dk>
2020-03-18 08:48:35 +0100

    Treid to changed the RFREader

--- Ladeskab/Moduls/IRFidReader.cs
+++ Ladeskab/Moduls/IRFidReader.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework.Internal.Execution;
+
 
 namespace Ladeskab.Moduls
 {
     public interface IRFidReader
     {
-        private event EventHandler<RFidChangedEventArgs> RFidChangedEvent;
+        event EventHandler<RFReadEventArgs> RFIDReadEvent;
+    }
 
-        void SetID(int newID);
+    public class RFIDReadEventArgs : EventArgs
+    {
+        public int ID { get; set; }
     }
 }
