cd1faa7803ffa1feb386e1089118788a0a26145f
Warmdahl <emilwarmdahl@gmail.com>
2020-03-25 09:41:15 +0100

    Prøver igen

--- Ladeskab_Test.Unit/TestLog.cs
+++ Ladeskab_Test.Unit/TestLog.cs
@@ -17,16 +17,41 @@ namespace Ladeskab_Test.Unit
             _log = new LogFile();
         }
 
-        [TestCase(true, 12)]
-        public void logfile(bool locked, int id)
+        /*[TestCase(true, 12)]
+        public void logfileTrue(bool locked, int id)
         {
             using (StringWriter sw = new StringWriter())
             {
-                
+
                 _log.Log(locked, id);
+                string s;
+                string st = "locked by: " + id + ", door is locked";
+                using (StreamReader SR = File.OpenText("navn.txt"))
+                {
+                    s = SR.ReadLine();
+                }
+
+                Assert.AreEqual(st, s);
+            }
+        } */
+        [TestCase(false, 10)]
+            public void logfileFalse(bool locked, int id)
+            {
+                using (StringWriter sw = new StringWriter())
+                {
+
+                    _log.Log(locked, id);
+                    string s;
+                    string st = "unlocked by: " + id + ", door is unlocked";
+                    using (StreamReader SR = File.OpenText("navn.txt"))
+                    {
+                        s = SR.ReadLine();
+                    }
+
+                    Assert.AreEqual(st, s);
+                }
             }
             
-        }
         
         
 
